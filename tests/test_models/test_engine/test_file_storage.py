0b507e73c1e675a0dde403b37de6acbdf988b316
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-14 23:44:27 +0200

    some tests of file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,3 +11,30 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
     """Test cases for the FileStorage class."""
+
+    def testObjectOfFileStorage(self):
+        """
+        test an object of FileStorage is instance of FileStorage class
+        """
+        self.assertEqual(type(FileStorage(), FileStorage))
+
+    def testNewMethod(self):
+        """
+        test new(self) method
+        """
+        b1 = BaseModel()
+        models.storage.new(b1)
+        self.assertIn("BaseModel.{}".format(b1.id), models.storage.all())
+
+    def testAll(self):
+        """
+        test all method
+        """
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def testNewMethodWithArgs(self):
+        """
+        test new method with more  than on args
+        """
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 1)
