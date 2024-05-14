98477d40e1bc0c60c5a6fbe69024863ba8a51419
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-14 10:01:43 +0200

    Add all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,21 @@
-from test import BaseModel
+from ..base_model import BaseModel
 
 class FileStorage:
+    """
+    This class provides functionality for storing and retrieving objects
+    using a file-based storage system. Objects are stored in a dictionary
+    format and serialized to a JSON file for persistence across program
+    executions.
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        Returns the dictionary __objects.
+
+        Returns:
+            dict: Dictionary containing all stored objects.
+        """
+        return self.__objects
         
