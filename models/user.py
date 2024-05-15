1126e5343232092d31b36bf791f1950d695e7b51
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-05-15 14:24:57 +0200

    bgn User class

---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class in هnherited from BaseModel class.
+    """
