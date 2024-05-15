fdb939bebfb6200cf05c39fec9a64befd1dfc15b
be-great <begreat5333@gmail.com>
2024-05-15 19:01:44 +0300

    update attribute types

--- models/place.py
+++ models/place.py
@@ -12,10 +12,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    number_rooms = ""
-    number_bathrooms = ""
-    max_guest = ""
-    price_by_night = ""
-    latitude = ""
-    longitude = ""
-    amenity_ids = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
