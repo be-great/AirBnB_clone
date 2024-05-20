ed6b283c43ff99f7b919d36224af9a2c5956a20b
be-great <begreat5333@gmail.com>
2024-05-20 03:02:22 +0300

    update console.py

--- console.py
+++ console.py
@@ -260,41 +260,36 @@ class HBNBCommand(cmd.Cmd):
             print(list)
 
     def do_update(self, arg):
-        """Usage: update <class> <id> <attribute_name> <attribute_value> or
-       <class>.update(<id>, <attribute_name>, <attribute_value>) or
-       <class>.update(<id>, <dictionary>)
-        Update a class instance of a given id by adding or updating
-        a given attribute key/value pair or dictionary."""
-        argl = parse(arg)
-        objdict = storage.all()
+        """Retrieve all instances or instances of a specific class."""
+        arguments = parsing(arg)
+        objs = storage.all()
 
         if len(arguments) < 1:
             print("** class name missing **")
             return False
-        if argl[0] not in HBNBCommand.__classes:
+        if arguments[0] not in self.__classnames:
             print("** class doesn't exist **")
             return False
-        if len(argl) == 1:
+        if len(arguments) < 2:
             print("** instance id missing **")
             return False
-        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+        if "{}.{}".format(arguments[0], arguments[1]) not in objs.keys():
             print("** no instance found **")
             return False
-        if len(argl) == 2:
+        if len(arguments) < 3:
             print("** attribute name missing **")
             return False
-        if len(argl) == 3:
+        if len(arguments) < 4:
             try:
                 type(eval(arguments[2])) != dict
             except NameError:
                 print("** value missing **")
                 return False
-
-        if len(argl) == 4:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            if argl[2] in obj.__class__.__dict__.keys():
-                valtype = type(obj.__class__.__dict__[argl[2]])
-                obj.__dict__[argl[2]] = valtype(argl[3])
+        if len(arguments) < 5:
+            obj = objs["{}.{}".format(arguments[0], arguments[1])]
+            if arguments[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[arguments[2]])
+                obj.__dict__[arguments[2]] = valtype(arguments[3])
             else:
                 obj.__dict__[arguments[2]] = arguments[3]
         elif type(eval(arguments[2])) == dict:
